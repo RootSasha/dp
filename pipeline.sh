e5d68ab3c771bf08a3e0c895c3b6a7aeb53c9552
Маньковський Олександр <117378642+RootSasha@users.noreply.github.com>
2025-03-27 12:02:03 +0200

    Update pipeline.sh

--- pipeline.sh
+++ pipeline.sh
@@ -1,60 +1,48 @@
 #!/bin/bash
 
-JENKINS_URL="http://localhost:8080"
-JENKINS_USER="admin"
-JENKINS_PASSWORD="1"
-CREDENTIAL_ID="ssh-key-jenkins"
-JOB_DIR="jenkins_jobs"
-CLI_JAR="jenkins_files/jenkins-cli.jar"
-
-mkdir -p "$JOB_DIR"
+source config.sh
 
 declare -A pipelines=(
-    ["grafana-monitoring"]="git@github.com:RootSasha/grafana.git"
-    ["monitoring-site"]="git@github.com:RootSasha/diplome-site.git"
+    ["grafana"]="git@github.com:RootSasha/grafana.git"
+    ["site"]="git@github.com:RootSasha/diplome-site.git"
 )
 
-# Перевірка, чи існує jenkins-cli.jar
-if [[ ! -f "$CLI_JAR" ]]; then
-    echo "❌ Помилка: Файл $CLI_JAR не знайдено. Завантаж його з Jenkins!"
-    exit 1
-fi
-
+# Створення або оновлення джобів Jenkins
 for job in "${!pipelines[@]}"; do
     REPO_URL="${pipelines[$job]}"
     JOB_XML="$JOB_DIR/$job.xml"
 
     echo "Створюємо пайплайн: $job (джерело: $REPO_URL)..."
 
+    mkdir -p "$JOB_DIR"
     cat > "$JOB_XML" <<EOF
 <flow-definition plugin="workflow-job">
-    <actions/>
-    <description>Pipeline для $job</description>
-    <keepDependencies>false</keepDependencies>
-    <properties/>
-    <definition class="org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition">
-        <scm class="hudson.plugins.git.GitSCM">
-            <configVersion>2</configVersion>
-            <userRemoteConfigs>
-                <hudson.plugins.git.UserRemoteConfig>
-                    <url>$REPO_URL</url>
-                    <credentialsId>$CREDENTIAL_ID</credentialsId>
-                </hudson.plugins.git.UserRemoteConfig>
-            </userRemoteConfigs>
-            <branches>
-                <hudson.plugins.git.BranchSpec>
-                    <name>*/main</name>
-                </hudson.plugins.git.BranchSpec>
-            </branches>
-        </scm>
-        <scriptPath>Jenkinsfile</scriptPath>
-        <sandbox>true</sandbox>
-    </definition>
-    <triggers/>
+        <actions/>
+        <description>Pipeline для $job</description>
+        <keepDependencies>false</keepDependencies>
+        <properties/>
+        <definition class="org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition">
+                <scm class="hudson.plugins.git.GitSCM">
+                        <configVersion>2</configVersion>
+                        <userRemoteConfigs>
+                                <hudson.plugins.git.UserRemoteConfig>
+                                        <url>$REPO_URL</url>
+                                        <credentialsId>$CREDENTIAL_ID</credentialsId>
+                                </hudson.plugins.git.UserRemoteConfig>
+                        </userRemoteConfigs>
+                        <branches>
+                                <hudson.plugins.git.BranchSpec>
+                                        <name>*/main</name>
+                                </hudson.plugins.git.BranchSpec>
+                        </branches>
+                </scm>
+                <scriptPath>Jenkinsfile</scriptPath>
+                <sandbox>true</sandbox>
+        </definition>
+        <triggers/>
 </flow-definition>
 EOF
 
-    # Створення або оновлення джоба
     java -jar "$CLI_JAR" -s "$JENKINS_URL" -auth "$JENKINS_USER:$JENKINS_PASSWORD" create-job "$job" < "$JOB_XML"
 
     if [[ $? -eq 0 ]]; then
